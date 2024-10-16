dc4503dcb26869b241e5899c962bab0e7070621e
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2024-10-16 08:49:10 -0400

    Add buttons in react, set up swa-db-connections folder

--- public/js/server.js
+++ public/js/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 const express = require('express'),
       sql = require('mssql');
       //cors = require('cors');
@@ -6,23 +6,9 @@ const express = require('express'),
 const app = express();
 //app.use(cors());
 
-const config = {
-    user: 'b8555f10-1bdc-47ed-a61f-86f02ae6d4d6',
-    password: 'Hgame$2016',
-    server: 'sqlserver-reactproject.database.windows.net',
-    database: 'adventureworks-aj',
-};
 
-app.get('/api/data', async (req, res) => {
-    try {
-        await sql.connect(config);
-        const result = await sql.query('SELECT TOP (100) FROM SalesLT.Customer');
-        res.send(result.recordset);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Database connection error');
-    }
-});
+
+
 
 app.listen(5000, () =>  {
     console.log('Server running on port 5000');
--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 
+function ListButton() {
+  function handleList() {
+    alert('You clicked list.');
+  }
+
+  return (
+    <button onClick={handleList}>List</button>
+  );
+}
+
+function GetButton() {
+  function handleGet() {
+    alert('You clicked get.');
+  }
+
+  return (
+    <button onClick={handleGet}>Get</button>
+  );
+}
+
+function UpdateButton() {
+  function handleUpdate() {
+    alert('You clicked update.');
+  }
+
+  return (
+    <button onClick={handleUpdate}>Update</button>
+  );
+}
+
+function CreateButton() {
+  function handleCreate() {
+    alert('You clicked create.');
+  }
+
+  return (
+    <button onClick={handleCreate}>Create</button>
+  );
+}
+
+function DeleteButton() {
+  function handleDelete() {
+    alert('You clicked delete.');
+  }
+
+  return (
+    <button onClick={handleDelete}>Delete</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Adventureworks CRUD</h1>
+        <ListButton />
+        <GetButton />
+        <UpdateButton />
+        <CreateButton />
+        <DeleteButton />
       </header>
     </div>
   );
