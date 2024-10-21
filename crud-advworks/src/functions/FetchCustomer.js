51abcdf85a747948e726ddeaa10bbd489e1ab626
ajayamoorthy <96484574+ajayamoorthy@users.noreply.github.com>
2024-10-20 20:58:01 -0400

    yuiop

--- crud-advworks/src/functions/FetchCustomer.js
+++ crud-advworks/src/functions/FetchCustomer.js
@@ -16,7 +16,7 @@ app.http('FetchCustomer', {
 
         try {
             //connect to database
-            await sql.connect('Server=tcp:sqlserver-reactproject.database.windows.net,1433;Initial Catalog=adventureworks-aj;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=Active Directory Managed Identity;');
+            await sql.connect("Server=tcp:sqlserver-reactproject.database.windows.net,1433;Initial Catalog=adventureworks-aj;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication='Active Directory Managed Identity';");
             
             //query the database for the customer by CustomerID
             const result = await sql.query`SELECT * FROM SalesLT.Customer WHERE CustomerID = ${customerID}`;
